c994e33c55daf51255dd15a17b8cf4e4cbfeb15b
Hana Mahmutović <113919501+mahmutovichana@users.noreply.github.com>
2023-09-29 12:27:56 +0200

    Update googleImageScraper.py

--- googleImageScraper.py
+++ googleImageScraper.py
@@ -10,7 +10,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-
 # Enter query for Google search
 query = "plastic"
 
